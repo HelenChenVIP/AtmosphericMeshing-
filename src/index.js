7d3f9c3ce1d67035709ad968227486e208016abf
houxfmark3955@163.com <houxfmark3955@163.com>
2018-07-31 10:13:01 +0800

    201807311012

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { AppRegistry, AsyncStorage } from 'react-native';
 import React from 'react'; 
 
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import { persistStore } from 'redux-persist';
 
 import dva from './utils/dva';
@@ -16,7 +16,7 @@ import api from './config/globalapi';
 
 
 
-const logger = createLogger();
+// const logger = createLogger();
 
 if (!__DEV__) {
   global.console = {
@@ -101,9 +101,11 @@ const app = dva({
   initialState: {},
   models: [],
   ...createLoading({ effects: true }),
-  onAction: [routerMiddleware, screenTracking, logger],
+  onAction: [routerMiddleware, screenTracking, 
+    // logger
+  ],
 });
-app.use(dvaEnhancer);
+// app.use(dvaEnhancer);
 registerModels(app);
 const App = app.start(<Router />);
 // persistStore(app.getStore(), {
